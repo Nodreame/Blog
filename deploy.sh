0d8d3cfd12a80997d5f2d6293d1b8032865d4056
Nodreame <nodreame94@gmail.com>
2021-04-07 04:35:00 +0800

    chore: CI with deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-# yarn docs:build
+yarn docs:build
 
 # navigate into the build output directory
 cd docs/.vitepress/dist
@@ -12,18 +12,16 @@ cd docs/.vitepress/dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 git init
-git config user.name "Travis CI"
-git config user.email "nodreame94@gmail.com"
+git config --local user.name "Travis CI"
+git config --local user.email "nodreame94@gmail.com"
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:Nodreame/Nodreame.github.io.git master
-git push -f "https://${GITHUB_TOKEN}@github.com/Nodreame/Nodreame.github.io.git" 
+git push -f https://${GITHUB_TOKEN}@github.com/Nodreame/Nodreame.github.io.git
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-# Cleanup
-cd ../..
-rm -rf .vuepress/dist
+cd -
