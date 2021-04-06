72cfe2132f68e3de1d98b6b9b386445593320b84
Nodreame <nodreame94@gmail.com>
2021-04-07 04:23:33 +0800

    chore: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,26 @@
 set -e
 
 # build
-npm run docs:build
+# yarn docs:build
 
 # navigate into the build output directory
 cd docs/.vitepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-
 git init
+git config user.name "Travis CI"
+git config user.email "nodreame94@gmail.com"
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:Nodreame/Nodreame.github.io.git master
+# git push -f git@github.com:Nodreame/Nodreame.github.io.git master
+git push -f "https://${GITHUB_TOKEN}@github.com/Nodreame/Nodreame.github.io.git" 
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-cd -
+# Cleanup
+cd ../..
+rm -rf .vuepress/dist
